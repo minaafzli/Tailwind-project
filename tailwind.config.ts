73bfc096780c001cdce040f1098805e59729034e
minaafzli <mina.afzli81@gmail.com>
2025-08-31 17:37:17 +0330

    first commit

---
+++ tailwind.config.ts
@@ -0,0 +1,11 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: ["./*html"],
+  theme: {
+    fontFamily: { Vazirmatn: ["Vazirmatn"] },
+  },
+  plugins: [],
+};
+
+export default config;
