2c03bbc23145bf970ff92d2b4904a5de8150f61f
minaafzli <mina.afzli81@gmail.com>
2025-09-01 23:13:31 +0330

    create footer

--- js/script.js
+++ js/script.js
@@ -1,20 +1,27 @@
-const tabs = document.querySelectorAll('.tab')
-const panels = document.querySelectorAll('.panel')
+const tabs = document.querySelectorAll(".tab");
+const panels = document.querySelectorAll(".panel");
 
-tabs.forEach((item) => item.addEventListener('click', onTabClick))
+tabs.forEach((item) => item.addEventListener("click", onTabClick));
 
 function onTabClick(e) {
   tabs.forEach((item) => {
-    item.children[0].classList.remove('border-b-4')
-  })
+    item.children[0].classList.remove("border-b-4");
+  });
 
-  panels.forEach((item) => item.classList.add('hidden'))
+  panels.forEach((item) => item.classList.add("hidden"));
 
-  e.target.classList.add('border-gray-400', 'border-b-4')
+  e.target.classList.add("border-gray-400", "border-b-4");
 
-  const classString = e.target.getAttribute('data-target')
+  const classString = e.target.getAttribute("data-target");
   document
-    .getElementById('panels')
+    .getElementById("panels")
     .getElementsByClassName(classString)[0]
-    .classList.remove('hidden')
+    .classList.remove("hidden");
 }
+
+function valid(email) {
+  const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return pattern.test(email);
+}
+const email = "example@domain.com";
+console.log(valid(email) ? "Valid email address" : "Invalid email address");
